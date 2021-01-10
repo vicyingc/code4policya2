11e36bac202a687e7fd5c4232488182a38d31be0
nkhlgeorge <nkhlgeorge@gmail.com>
2021-01-10 20:26:41 +0530

    updated python file to create dictionary

--- create_dictionary.py
+++ create_dictionary.py
@@ -3,4 +3,4 @@
 import pandas as pd
 df = pd.read_csv('OxCGRT_latest_onerowpercountry_01092021_0100.csv')
 df.columns
-df[['CountryName', 'C5_Close public transport', 'C8_International travel controls', 'H6_Facial Coverings']].head().to_json('data.json',orient='records')
+df[['CountryName', 'C5_Close public transport', 'C8_International travel controls', 'H6_Facial Coverings']].to_json('data.json',orient='records')
