72e5e2b2a559814b17144d1fd82c987e37f2a428
vicyingc <65056439+vicyingc@users.noreply.github.com>
2021-01-12 21:35:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
- make what will later be visualization instances global variables
+//make what will later be visualization instances global variables
 // alert(sammy.first_name);
 var sammy
 var first_name
